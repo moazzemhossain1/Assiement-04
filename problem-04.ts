9e1967cb1fd6bfcbb3f9562c4afc2b4f55e220f8
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-08-24 08:14:09 +0800

    problem -04.ts

---
+++ problem-04.ts
@@ -0,0 +1,16 @@
+
+
+type  Light="red" | "yellow" | "green";
+
+
+
+function getTrafficAction(light:Light):string {
+    if(light==="red"){
+        return "Stop"
+    }else if(light === "yellow"){
+        return "Slow Down"
+    }else{
+        return "Go"
+    }
+    
+}
